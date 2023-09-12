2318b5f1a58b2c9ccd8407fb8a7f8898f6a6a325
Abu-0-RayhaN <rayhan844625@gmail.com>
2023-09-12 22:15:24 +0600

    some js for superb bg

---
+++ index.js
@@ -0,0 +1,14 @@
+const body = document.body;
+
+        body.addEventListener('mousemove', (e) => {
+            const x = e.clientX / window.innerWidth;
+            const y = e.clientY / window.innerHeight;
+
+            const red = Math.round(x * 255);
+            const green = Math.round(y * 255);
+            const blue = 100; // You can set your preferred value here
+
+            const color = `rgb(${red},${green},${blue})`;
+
+            body.style.backgroundColor = color;
+        });
